91ef32bbdad74c90ecf80e189ee6cec4e4c5b804
Alexandar Galabov <alexandar_1997@abv.bg>
2019-11-23 23:28:33 +0200

    Change import quotes to single quote.

--- web-app/src/Components/Navbar/index.js
+++ web-app/src/Components/Navbar/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import View from "../UI/View";
-import "./styles.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import View from '../UI/View';
+import './styles.css';
 
 const Navbar = () => {
-  const title = "Bulgaria Guide";
+  const title = 'Bulgaria Guide';
 
   return (
     <nav className="nav-wrapper blue" id="sticky">
       <View className="container">
-        <a className="brand-logo">{title}</a>
+        <a href="/home" className="brand-logo">{title}</a>
         <ul className="right">
           <li>
             <NavLink to="/home">Начало</NavLink>
--- web-app/src/Components/UI/View/index.js
+++ web-app/src/Components/UI/View/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const View = props => (
   <div className={props.className} style={props.style}>
