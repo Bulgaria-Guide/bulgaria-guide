52bc7393b408426d7f725ee2eb699a3e4cc0ebaa
Alexandar Galabov <alexandar_1997@abv.bg>
2019-11-23 16:04:50 +0200

    Add custom components into main App entry.

---
+++ web-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Components/Navbar/index';
+import Home from './Components/Home';
+import SightsList from './Components/Sights/List';
+import Login from './Components/Login';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const App = () => (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/sights" component={SightsList} />
+        <Route path="/login" component={Login} />
+      </div>
+    </BrowserRouter>
+  );
+
+export default App;
