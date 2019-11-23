623713b536186fa3bae74aaab42cd0e59bf3bbf7
Alexandar Galabov <alexandar_1997@abv.bg>
2019-11-23 18:12:07 +0200

    Install prettier.

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Navbar from './Components/Navbar/index';
-import Home from './Components/Home';
-import SightsList from './Components/Sights/List';
-import Login from './Components/Login';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import Navbar from "./Components/Navbar/index";
+import Home from "./Components/Home";
+import SightsList from "./Components/Sights/List";
+import SightDetails from "./Components/Sights/Details";
+import Login from "./Components/Login";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => (
-    <BrowserRouter>
-      <div>
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/sights" component={SightsList} />
-        <Route path="/login" component={Login} />
-      </div>
-    </BrowserRouter>
-  );
+  <BrowserRouter>
+    <Navbar />
+    <Route exact path="/" component={Home} />
+    <Route path="/home" component={Home} />
+    <Route path="/sights" component={SightsList} />
+    <Route path="/details" component={SightDetails} />
+    <Route path="/login" component={Login} />
+  </BrowserRouter>
+);
 
 export default App;
