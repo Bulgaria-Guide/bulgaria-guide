407d4504856b8cfbdaa08c90eb60c87309322175
Alexandar Galabov <alexandar_1997@abv.bg>
2019-11-23 18:11:17 +0200

    Use ContainerLayout in other pages.

--- web-app/src/Components/Login/index.js
+++ web-app/src/Components/Login/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import Text from '../UI/Text';
-import View from '../UI/View';
+import React from "react";
+import Text from "../UI/Text";
+import ContainerLayout from "../UI/ContainerLayout";
 
 const Login = () => (
-    <View className="container">
-      <h1 className="center">Login</h1>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla
-        convallis lorem eget mollis. Aliquam volutpat mollis risus, ut tincidunt
-        dui mattis venenatis. Etiam ultrices, nunc ullamcorper aliquet lobortis,
-        velit dolor iaculis lacus, at iaculis est mauris vitae libero. Nulla
-        aliquam dolor a velit fermentum, non ultrices elit efficitur. Ut eu
-        dictum purus. Aliquam eleifend tempor nisl, ac mollis orci consequat in.
-        Proin eget lectus pharetra, tempus justo eget, porta purus. Etiam mollis
-        molestie tempus. Donec quis ex non felis porttitor blandit non id nisl.
-        Pellentesque lobortis sed nulla vitae bibendum. Ut rhoncus posuere
-        sapien a lobortis. Pellentesque euismod orci non scelerisque molestie.
-        Aliquam posuere condimentum enim sed posuere. Vivamus lacinia nisi in
-        orci suscipit, at aliquet sem tincidunt.
-      </Text>
-    </View>
-  );
+  <ContainerLayout header="Login">
+    <Text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla
+      convallis lorem eget mollis. Aliquam volutpat mollis risus, ut tincidunt
+      dui mattis venenatis. Etiam ultrices, nunc ullamcorper aliquet lobortis,
+      velit dolor iaculis lacus, at iaculis est mauris vitae libero. Nulla
+      aliquam dolor a velit fermentum, non ultrices elit efficitur. Ut eu dictum
+      purus. Aliquam eleifend tempor nisl, ac mollis orci consequat in. Proin
+      eget lectus pharetra, tempus justo eget, porta purus. Etiam mollis
+      molestie tempus. Donec quis ex non felis porttitor blandit non id nisl.
+      Pellentesque lobortis sed nulla vitae bibendum. Ut rhoncus posuere sapien
+      a lobortis. Pellentesque euismod orci non scelerisque molestie. Aliquam
+      posuere condimentum enim sed posuere. Vivamus lacinia nisi in orci
+      suscipit, at aliquet sem tincidunt.
+    </Text>
+  </ContainerLayout>
+);
 
 export default Login;
--- web-app/src/Components/Navbar/index.js
+++ web-app/src/Components/Navbar/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
-import View from '../UI/View';
-import './styles.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import View from "../UI/View";
+import "./styles.css";
 
 const Navbar = () => {
-  const title = 'Bulgaria Guide';
+  const title = "Bulgaria Guide";
 
   return (
-    <nav className="nav-wrapper blue">
+    <nav className="nav-wrapper blue" id="sticky">
       <View className="container">
         <a className="brand-logo">{title}</a>
         <ul className="right">
           <li>
-            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/home">Начало</NavLink>
           </li>
           <li>
-            <NavLink to="/sights">Sights</NavLink>
+            <NavLink to="/sights">Обекти</NavLink>
           </li>
           <li>
-            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/details">Обект*</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Вход</NavLink>
           </li>
         </ul>
       </View>
