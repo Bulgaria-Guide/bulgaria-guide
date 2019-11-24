c0d30a3fd48da60ca4b0b70670a76144493e7ee3
Alexandar Galabov <alexandar_1997@abv.bg>
2019-11-24 20:28:20 +0200

    Run lint-fix for indentation problems

--- web-app/src/Components/Sights/List/Card/index.js
+++ web-app/src/Components/Sights/List/Card/index.js
@@ -3,31 +3,31 @@ import { NavLink } from 'react-router-dom';
 import Text from '../../../UI/Text';
 
 const SightCard = props => (
-    <div className="col s12 m7">
-      <div className="card horizontal">
-        <div className="card-image">
-          <img
-            src="https://lorempixel.com/100/190/nature/6"
-            height="100%"
-            alt={props.sight.name}
-          />
+  <div className="col s12 m7">
+    <div className="card horizontal">
+      <div className="card-image">
+        <img
+          src="https://lorempixel.com/100/190/nature/6"
+          height="100%"
+          alt={props.sight.name}
+        />
+      </div>
+      <div className="card-stacked">
+        <div className="card-content" style={{ 'paddingTop': 0 }}>
+          <h4 className="header">{`${props.sight.name}`}</h4>
+          <Text>{props.sight.description.substr(0, 135).concat('...')}</Text>
         </div>
-        <div className="card-stacked">
-          <div className="card-content" style={{ 'paddingTop': 0 }}>
-            <h4 className="header">{`${props.sight.name}`}</h4>
-            <Text>{props.sight.description.substr(0, 135).concat('...')}</Text>
-          </div>
-          <div className="card-action">
-            <NavLink to={{
-              'pathname': '/details',
-              'sight': { ...props.sight }
-            }}>
+        <div className="card-action">
+          <NavLink to={{
+            'pathname': '/details',
+            'sight': { ...props.sight }
+          }}>
               Вижте още
-            </NavLink>
-          </div>
+          </NavLink>
         </div>
       </div>
     </div>
+  </div>
 );
 
 export default SightCard;
