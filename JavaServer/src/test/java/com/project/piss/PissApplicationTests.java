d64994828effe1b2fe4f4977c5f671a4a95393e1
TodorAngelov97 <tangelov@tangelov-a01.vmware.com>
2019-11-25 21:53:19 +0200

    Initial commit

---
+++ JavaServer/src/test/java/com/project/piss/PissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.piss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
