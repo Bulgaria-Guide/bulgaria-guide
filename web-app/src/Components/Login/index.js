75036aa8255c5d329306c14e3972995559dcf98a
Niya Raykova <niq_raikova@abv.bg>
2019-11-30 20:33:41 +0200

    login form

--- web-app/src/Components/Login/index.js
+++ web-app/src/Components/Login/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import Text from '../UI/Text';
+import LoginForm from '../Login/Form';
 import ContainerLayout from '../UI/ContainerLayout';
 
 const Login = () => (
-  <ContainerLayout header="Login">
+  <ContainerLayout >
     <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla
-      convallis lorem eget mollis. Aliquam volutpat mollis risus, ut tincidunt
-      dui mattis venenatis. Etiam ultrices, nunc ullamcorper aliquet lobortis,
-      velit dolor iaculis lacus, at iaculis est mauris vitae libero. Nulla
-      aliquam dolor a velit fermentum, non ultrices elit efficitur. Ut eu dictum
-      purus. Aliquam eleifend tempor nisl, ac mollis orci consequat in. Proin
-      eget lectus pharetra, tempus justo eget, porta purus. Etiam mollis
-      molestie tempus. Donec quis ex non felis porttitor blandit non id nisl.
-      Pellentesque lobortis sed nulla vitae bibendum. Ut rhoncus posuere sapien
-      a lobortis. Pellentesque euismod orci non scelerisque molestie. Aliquam
-      posuere condimentum enim sed posuere. Vivamus lacinia nisi in orci
-      suscipit, at aliquet sem tincidunt.
+  
     </Text>
+<LoginForm />
   </ContainerLayout>
+  
 );
 
 export default Login;
+
