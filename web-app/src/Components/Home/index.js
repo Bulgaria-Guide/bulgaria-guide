3359d1fbad59961837a2a7974c5ea87ab0bf393a
Alexandar Galabov <alexandar_1997@abv.bg>
2019-11-30 23:06:46 +0200

    Add simple tests for authentication

--- web-app/src/Components/Home/index.js
+++ web-app/src/Components/Home/index.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Text from '../UI/Text';
 import ContainerLayout from '../UI/ContainerLayout';
 import SearchBar from '../Seach/Bar';
+import useRole from '../../hooks/useRole';
 
-const Home = () => (
-  <ContainerLayout header="Home">
-    <SearchBar />
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla
-      convallis lorem eget mollis. Aliquam volutpat mollis risus, ut tincidunt
-      dui mattis venenatis. Etiam ultrices, nunc ullamcorper aliquet lobortis,
-      velit dolor iaculis lacus, at iaculis est mauris vitae libero. Nulla
-      aliquam dolor a velit fermentum, non ultrices elit efficitur. Ut eu dictum
-      purus. Aliquam eleifend tempor nisl, ac mollis orci consequat in. Proin
-      eget lectus pharetra, tempus justo eget, porta purus. Etiam mollis
-      molestie tempus. Donec quis ex non felis porttitor blandit non id nisl.
-      Pellentesque lobortis sed nulla vitae bibendum. Ut rhoncus posuere sapien
-      a lobortis. Pellentesque euismod orci non scelerisque molestie. Aliquam
-      posuere condimentum enim sed posuere. Vivamus lacinia nisi in orci
-      suscipit, at aliquet sem tincidunt.
-    </Text>
-  </ContainerLayout>
-);
+const Home = () => {
+  const { isLoggedIn } = useRole();
+
+  const roleText = useMemo(
+    () => (isLoggedIn
+      ? 'Hello logged user'
+      : 'You can sign in'),
+    [isLoggedIn]
+  );
+
+  return (
+    <ContainerLayout header="Home">
+      <SearchBar />
+      {roleText}
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla
+        convallis lorem eget mollis. Aliquam volutpat mollis risus, ut tincidunt
+        dui mattis venenatis. Etiam ultrices, nunc ullamcorper aliquet lobortis,
+        velit dolor iaculis lacus, at iaculis est mauris vitae libero. Nulla
+        aliquam dolor a velit fermentum, non ultrices elit efficitur. Ut eu dictum
+        purus. Aliquam eleifend tempor nisl, ac mollis orci consequat in. Proin
+        eget lectus pharetra, tempus justo eget, porta purus. Etiam mollis
+        molestie tempus. Donec quis ex non felis porttitor blandit non id nisl.
+        Pellentesque lobortis sed nulla vitae bibendum. Ut rhoncus posuere sapien
+        a lobortis. Pellentesque euismod orci non scelerisque molestie. Aliquam
+        posuere condimentum enim sed posuere. Vivamus lacinia nisi in orci
+        suscipit, at aliquet sem tincidunt.
+      </Text>
+    </ContainerLayout>
+  );
+};
 
 export default Home;
--- web-app/src/Components/Login/index.js
+++ web-app/src/Components/Login/index.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Text from '../UI/Text';
 import ContainerLayout from '../UI/ContainerLayout';
+import { AccountContext } from '../../context/account';
 
-const Login = () => (
-  <ContainerLayout header="Login">
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla
-      convallis lorem eget mollis. Aliquam volutpat mollis risus, ut tincidunt
-      dui mattis venenatis. Etiam ultrices, nunc ullamcorper aliquet lobortis,
-      velit dolor iaculis lacus, at iaculis est mauris vitae libero. Nulla
-      aliquam dolor a velit fermentum, non ultrices elit efficitur. Ut eu dictum
-      purus. Aliquam eleifend tempor nisl, ac mollis orci consequat in. Proin
-      eget lectus pharetra, tempus justo eget, porta purus. Etiam mollis
-      molestie tempus. Donec quis ex non felis porttitor blandit non id nisl.
-      Pellentesque lobortis sed nulla vitae bibendum. Ut rhoncus posuere sapien
-      a lobortis. Pellentesque euismod orci non scelerisque molestie. Aliquam
-      posuere condimentum enim sed posuere. Vivamus lacinia nisi in orci
-      suscipit, at aliquet sem tincidunt.
-    </Text>
-  </ContainerLayout>
-);
+const Login = () => {
+  const { role, setRole } = useContext(AccountContext);
+
+  return (
+    <ContainerLayout header="Login">
+      <button disabled={role === 'guest'} onClick={() => setRole('guest')}>Go Guest</button>
+      <button disabled={role === 'user'} onClick={() => setRole('user')}>Go User</button>
+      <button disabled={role === 'admin'} onClick={() => setRole('admin')}>Go Admin</button>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla
+        convallis lorem eget mollis. Aliquam volutpat mollis risus, ut tincidunt
+        dui mattis venenatis. Etiam ultrices, nunc ullamcorper aliquet lobortis,
+        velit dolor iaculis lacus, at iaculis est mauris vitae libero. Nulla
+        aliquam dolor a velit fermentum, non ultrices elit efficitur. Ut eu dictum
+        purus. Aliquam eleifend tempor nisl, ac mollis orci consequat in. Proin
+        eget lectus pharetra, tempus justo eget, porta purus. Etiam mollis
+        molestie tempus. Donec quis ex non felis porttitor blandit non id nisl.
+        Pellentesque lobortis sed nulla vitae bibendum. Ut rhoncus posuere sapien
+        a lobortis. Pellentesque euismod orci non scelerisque molestie. Aliquam
+        posuere condimentum enim sed posuere. Vivamus lacinia nisi in orci
+        suscipit, at aliquet sem tincidunt.
+      </Text>
+    </ContainerLayout>
+  );
+};
 
 export default Login;
