7bd41c2f3536e9bd8edb465a95c765589e68af2f
Alexandar Galabov <44395908+AGalabov@users.noreply.github.com>
2019-12-01 20:58:06 +0200

    Show optional fields depending on role (#15)
    * Allow cleaner imports
    
    * Create button to delete comment or sight

--- web-app/src/Components/Sights/Details/Comments/Card/index.js
+++ web-app/src/Components/Sights/Details/Comments/Card/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import Text from '../../../../UI/Text';
+import Text from 'Components/UI/Text';
+import useRole from 'hooks/useRole';
+import FloatingButton from 'Components/UI/Button/Floating';
 
-const CommentCard = props => (
-  <div className="col s12 m7">
-    <div className="card horizontal">
-      <div className="card-stacked">
-        <div className="card-action grey lighten-1" style={{ 'paddingTop': 0,
-          'paddingBottom': 0 }}>
-          <Text className="header">{props.comment.authorName}</Text>
-        </div>
-        <div className="card-content grey lighten-4" >
-          <Text>{props.comment.content}</Text>
+const CommentCard = props => {
+  const { isAdmin } = useRole();
+
+  const deleteComment = () => {
+    // APIClient.deleteComment(props.comment.id);
+  }
+
+  return (
+    <div className="col s12 m7">
+      <div className="card horizontal">
+        <div className="card-stacked">
+          <FloatingButton to='/home' onClick={deleteComment} label='X' style={{ top: 0, right: 0 }} />
+          <div className="card-action grey lighten-1" style={{
+            'paddingTop': 0,
+            'paddingBottom': 0
+          }}>
+            <Text className="header">{props.comment.authorName}</Text>
+          </div>
+          <div className="card-content grey lighten-4" >
+            <Text>{props.comment.content}</Text>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
-
+    </div >
+  );
+}
 export default CommentCard;
