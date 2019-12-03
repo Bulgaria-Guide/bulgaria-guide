05d31b5632334ba2e8389992ff8006a9cfc591fc
Alexandar Galabov <alexandar_1997@abv.bg>
2019-12-04 01:29:20 +0200

    Make login form usable

--- web-app/src/Components/Login/Form/index.js
+++ web-app/src/Components/Login/Form/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './styles.css';
+import useTextField from 'hooks/fields/useTextField';
+import APIClient from 'ApiClient';
 
-const LoginForm = () => (
-  <div className="loginbox">
-    <h1>Влизане</h1>
-    <form>
-      <p>Потребител</p>
-      <input type="text" name="" placeholder="Въведете потребителско име" />
-      <p>Парола</p>
-      <input type="password" name="" placeholder="Въведете парола" />
-      <input type="submit" name="" value="Влизане" />
-    </form>
-  </div>
+const LoginForm = () => {
+  const usernameField = useTextField();
+  const passwordField = useTextField();
 
-);
+  const handleSubmit = useCallback(event => {
+    const data = {
+      'username': usernameField.content,
+      'password': passwordField.content
+    };
+    console.log(data);
+    APIClient.login(data);
+    event.preventDefault();
+  }, [passwordField.content, usernameField.content]);
+
+  return (
+    <div className="loginbox">
+      <h1>Влизане</h1>
+      <form onSubmit={handleSubmit}>
+        <p>Потребител</p>
+        <input
+          type="text"
+          value={usernameField.content}
+          onChange={usernameField.handleChange}
+          placeholder="Въведете потребителско име" />
+        <p>Парола</p>
+        <input
+          type="password"
+          value={passwordField.content}
+          onChange={passwordField.handleChange}
+          placeholder="Въведете парола" />
+        <input type="submit" value="Влизане" />
+      </form>
+    </div>
+  );
+};
 export default LoginForm;
