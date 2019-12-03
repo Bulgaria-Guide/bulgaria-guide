7fd73c724f2aadd577b7a72c3576ab2657ddbdfd
Alexandar Galabov <alexandar_1997@abv.bg>
2019-12-04 01:28:51 +0200

    Make add comment form usable

--- web-app/src/Components/Sights/Details/Comments/New/index.js
+++ web-app/src/Components/Sights/Details/Comments/New/index.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import APIClient from 'ApiClient';
+import useTextField from 'hooks/fields/useTextField';
 
-const NewCommentField = () => (
-  <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s12">
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-          <label
-            className="active"
-            style={{
-              'background-color': 'transparent',
-              'paddingLeft': 10
-            }}
-            for="textarea1">Напишете вашият коментар
-          </label>
-          <a className="waves-effect waves-light btn" style={{ 'zIndex': 0 }} id="button">
-            Постави
-          </a>
+const NewCommentField = sightId => {
+
+  const { content, handleChange } = useTextField();
+  const handleSubmit = useCallback(event => {
+    console.log(content);
+    APIClient.addSightComment(sightId, content);
+    event.preventDefault();
+  }, [content, sightId]);
+
+  return (
+    <div className="row">
+      <form className="col s12" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="input-field col s12">
+            <textarea
+              value={content}
+              onChange={handleChange}
+              id="textarea1"
+              className="materialize-textarea" />
+            <label
+              className="active"
+              style={{
+                'backgroundColor': 'transparent',
+                'paddingLeft': 10
+              }}
+              htmlFor="textarea1">Напишете вашият коментар
+            </label>
+            <input
+              className="waves-effect waves-light btn"
+              style={{ 'zIndex': 0 }}
+              type="submit"
+              value="Постави" />
+          </div>
         </div>
-      </div>
-    </form>
-  </div >
-);
+      </form>
+    </div >
+  );
+};
 export default NewCommentField;
