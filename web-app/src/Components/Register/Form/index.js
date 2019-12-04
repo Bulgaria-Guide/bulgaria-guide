09fc74ff6d1d99f2928b832c94bf2a802b72c2b3
Alexandar Galabov <alexandar_1997@abv.bg>
2019-12-04 21:18:14 +0200

    Validate register form and send request data

--- web-app/src/Components/Register/Form/index.js
+++ web-app/src/Components/Register/Form/index.js
@@ -1,18 +1,72 @@
-import React, { Component } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './styles.css';
+import APIClient from 'ApiClient';
+import useField from 'hooks/useField';
 
-const RegisterForm = () => (
-  <div class="registerbox">
-    <h1>Регистрация</h1>
-    <form>
-      <p>Име</p>
-      <input type="text" name="" placeholder="Име" />
-      <p>Парола</p>
-      <input type="password" name="" placeholder="Парола"/>
-      <p>Повтори паролата</p>
-      <input type="password" name="" placeholder="Повтори паролата"/>
-      <input type="submit" name="" value="Регистрация"/>
-    </form>
-  </div>
-);
+const RegisterForm = () => {
+  const usernameField = useField('');
+  const passwordField = useField('');
+  const confirmPasswordField = useField('');
+
+  const [formError, setFormError] = useState('');
+  const [formWasSent, setFormWasSent] = useState(false);
+
+  const isFormValid = useMemo(() => {
+    if (usernameField.content.length < 1 || passwordField.content.length < 1) {
+      setFormError('Потребителското име и парола са задължителни');
+      return false;
+    }
+    if (passwordField.content.length < 6) {
+      setFormError('Паролата трябва да е поне 6 символа');
+      return false;
+    }
+    if (passwordField.content !== confirmPasswordField.content) {
+      setFormError('Паролите трябва да съвпадат');
+      return false;
+    }
+    setFormError('');
+    return true;
+  }, [confirmPasswordField.content, passwordField.content, usernameField.content.length]);
+
+  const handleSubmit = useCallback(event => {
+    setFormWasSent(true);
+    if (isFormValid) {
+      const data = {
+        'username': usernameField.content,
+        'password': passwordField.content
+      };
+      console.log(data);
+      APIClient.register(data);
+    }
+    event.preventDefault();
+  }, [isFormValid, passwordField.content, usernameField.content]);
+
+  return (
+    <div class="registerbox">
+      <h1>Регистрация</h1>
+      <form onSubmit={handleSubmit}>
+        <p>Име</p>
+        <input
+          type="text"
+          value={usernameField.content}
+          onChange={usernameField.handleChange}
+          placeholder="Име" />
+        <p>Парола</p>
+        <input
+          type="password"
+          value={passwordField.content}
+          onChange={passwordField.handleChange}
+          placeholder="Парола" />
+        <p>Повтори паролата</p>
+        <input
+          type="password"
+          value={confirmPasswordField.content}
+          onChange={confirmPasswordField.handleChange}
+          placeholder="Повтори паролата" />
+        <input type="submit" value="Регистрация" />
+      </form>
+      {formWasSent && <p className="errorMessage">{formError}</p>}
+    </div>
+  );
+};
 export default RegisterForm;
--- web-app/src/Components/Register/index.js
+++ web-app/src/Components/Register/index.js
@@ -1,18 +1,12 @@
-import React, { useContext } from 'react';
-import Text from '../UI/Text';
+import React from 'react';
 import RegisterForm from './Form';
 import ContainerLayout from '../UI/ContainerLayout';
-import { AccountContext } from '../../context/account';
 
-const Register = () => {
-  const { role, setRole } = useContext(AccountContext);
-
-  return (
-    <ContainerLayout header="Login">
-      <RegisterForm />
-    </ContainerLayout>
-  );
-};
+const Register = () => (
+  <ContainerLayout header="Register">
+    <RegisterForm />
+  </ContainerLayout>
+);
 
 export default Register;
 
