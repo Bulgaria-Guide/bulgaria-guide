4f8ce61a4ec189a4ba7b6669ce389ae1a6b56af5
Alexandar Galabov <alexandar_1997@abv.bg>
2019-12-05 01:09:32 +0200

    Make results of Search props sights for List

--- web-app/src/Components/Sights/List/index.js
+++ web-app/src/Components/Sights/List/index.js
@@ -3,25 +3,11 @@ import SightCard from './Card';
 import View from '../../UI/View';
 import PendingSightCard from './Pending';
 import constants from 'resources/constants';
-// import APIClient from 'ApiClient';
 
 const sights = constants.sights.list;
 
+// function SightsList({ sights, pending = false }) {
 function SightsList({ pending = false }) {
-
-  // const [sights, setSights] = useState([]);
-
-  // useEffect(() => {
-  //   APIClient.getAllSights().then(data => {
-  //     console.log(data);
-  //     setSights(data);
-  //   })
-  //     .catch(err => {
-  //       console.log(err);
-
-  //       return null;
-  //     });
-  // }, [setSights]);
 
   const renderSight = useCallback(sight => (pending
     ? <PendingSightCard sight={sight} key={sight.id} />
