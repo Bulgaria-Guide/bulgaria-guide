3f30d495e9546a02bb0538de8cc25268c1a44b8c
Alexandar Galabov <alexandar_1997@abv.bg>
2019-12-05 01:11:03 +0200

    Send pending sights from API call as props to List

--- web-app/src/Components/Administration/index.js
+++ web-app/src/Components/Administration/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContainerLayout from '../UI/ContainerLayout';
 import SightsList from '../Sights/List';
+import APIClient from 'ApiClient';
 
-const Administration = () => (
-  <ContainerLayout header={'Администрация'}>
-    <SightsList pending/>
-  </ContainerLayout>
-);
+const Administration = () => {
+  const [pendingSights, setPendingSights] = useState([]);
+
+  useEffect(() => {
+    APIClient.getPendingSights()
+      .then(res => {
+        console.log(res);
+        setPendingSights(res);
+      })
+      .catch(err => {
+        console.log(err);
+        return null;
+      });
+  }, []);
+  return (
+    <ContainerLayout header={'Администрация'}>
+      <SightsList sights={pendingSights} pending />
+    </ContainerLayout>
+  );
+};
 
 export default Administration;
