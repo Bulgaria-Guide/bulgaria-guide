111d2d4116b2592d5133e229bb2badd006a64613
Alexandar Galabov <alexandar_1997@abv.bg>
2019-12-15 12:38:35 +0200

    Add api call and refactor new sight form

--- web-app/src/Components/Sights/New/Form/index.js
+++ web-app/src/Components/Sights/New/Form/index.js
@@ -1,106 +1,78 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { Redirect } from 'react-router-dom';
+import useAccount from 'hooks/useAccount';
+import TextField from 'Components/UI/Field/Text';
+import APIClient from 'ApiClient';
 
-const SightForm = () => (
-  <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div className="input-field col s12">
-          <textarea
-            id="textarea1"
-            className="materialize-textarea" />
-          <label
-            className="active"
-            style={{
-              'backgroundColor': 'transparent',
-              'paddingLeft': 0
-            }}
-            htmlFor="textarea1">Име
-          </label>
+const SightForm = () => {
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+  const [hasFormError, setHasFormError] = useState(false);
+  const { authToken } = useAccount();
+
+  const handleSubmit = useCallback(event => {
+    event.preventDefault();
+    console.log(event.target);
+    const data = new FormData(event.target);
+    const price = parseFloat(data.get('price'));
+    data.set('price', price);
+    const longitude = parseFloat(data.get('longitude'));
+    data.set('longitude', longitude);
+    const latitude = parseFloat(data.get('latitude'));
+    data.set('latitude', latitude);
+
+    APIClient.createSight(data, authToken)
+      .then(() => setShouldRedirect(true))
+      .catch(() => setHasFormError(true));
+
+  }, [authToken]);
+
+  return (
+    <div className="row">
+      <form
+        className="col s12"
+        onSubmit={handleSubmit}>
+        <div className="row">
+          <TextField fieldName="name" label="Име" />
+          <label>Изберете категория</label>
+          <select
+            defaultValue=""
+            name="category"
+            className="browser-default">
+            <option value="" disabled>Изберете категория:</option>
+            <option value="historical">Исторически</option>
+            <option value="culture">Културни</option>
+            <option value="sports">Спортни</option>
+            <option value="rest">Отдих</option>
+          </select>
+          <TextField fieldName="address" label="Адрес" />
+          <TextField fieldName="longitude" label="Longitude" />
+          <TextField fieldName="latitude" label="Latitude" />
+          <TextField fieldName="workingTimeFrom" label="Работно време от:" />
+          <TextField fieldName="workingTimeTo" label="Работно време до:" />
+          <TextField fieldName="price" label="Цена:" />
+          <TextField fieldName="description" label="Описание:" />
+          <div>
+            <span>Снимка: </span>
+            <input type="file" name="picture" />
+          </div>
+          {hasFormError && <p style={{ 'color': 'red' }}>Моля попълнете коректно всички полета</p>}
+          <div className="input-field col s9">
+            <input
+              className="waves-effect waves-light btn-large"
+              style={{
+                'zIndex': 0,
+                'flex': 1
+              }}
+              id="button"
+              type="submit"
+              value="ДОБАВИ"
+            />
+          </div>
         </div>
-        <div className="input-field col s12">
-          <textarea
-            id="textarea1"
-            className="materialize-textarea" />
-          <label
-            className="active"
-            style={{
-              'backgroundColor': 'transparent',
-              'paddingLeft': 0
-            }}
-            htmlFor="textarea1">Адрес
-          </label>
-        </div>
-        <div className="input-field col s12">
-          <textarea
-            id="textarea1"
-            className="materialize-textarea" />
-          <label
-            className="active"
-            style={{
-              'backgroundColor': 'transparent',
-              'paddingLeft': 0
-            }}
-            htmlFor="textarea1">Работно време от:
-          </label>
-        </div>
-        <div className="input-field col s12">
-          <textarea
-            id="textarea1"
-            className="materialize-textarea" />
-          <label
-            className="active"
-            style={{
-              'backgroundColor': 'transparent',
-              'paddingLeft': 0
-            }}
-            htmlFor="textarea1">Работно време до:
-          </label>
-        </div>
-        <div className="input-field col s12">
-          <textarea
-            id="textarea1"
-            className="materialize-textarea" />
-          <label
-            className="active"
-            style={{
-              'backgroundColor': 'transparent',
-              'paddingLeft': 0
-            }}
-            htmlFor="textarea1">Цена
-          </label>
-        </div>
-        <div className="input-field col s12">
-          <textarea
-            id="textarea1"
-            className="materialize-textarea" />
-          <label
-            className="active"
-            style={{
-              'backgroundColor': 'transparent',
-              'font-size': '55px;',
-              'paddingLeft': 0
-            }}
-            htmlFor="textarea1">Описание
-          </label>
-        </div>
-      </div>
-      <div class="btn">
-        <span>Снимка </span>
-        <input type="file" />
-      </div>
-      <div class="file-path-wrapper">
-        <input class="file-path validate" type="text" />
-      </div>
-      <label>Изберете категория</label>
-      <select class="browser-default">
-        <option value="" disabled selected>Изберете категория:</option>
-        <option value="all">Всички</option>
-        <option value="historical">Исторически</option>
-        <option value="culture">Културни</option>
-        <option value="sports">Спортни</option>
-        <option value="rest">Отдих</option>
-      </select>
-    </form>
-  </div>
-);
+      </form>
+      {shouldRedirect && <Redirect to="/home" />}
+    </div>
+  );
+};
+
 export default SightForm;
